3662cd096f0f8ebc2c9b2e8eb7ef6c56fc62aa55
杨森 <564018929@qq.com>
2019-11-19 18:03:37 +0800

    add RowMatrix codes

--- src/main/scala/com/mathartsys/yss/sparkmllib/MllibTest.scala
+++ src/main/scala/com/mathartsys/yss/sparkmllib/MllibTest.scala
@@ -6,6 +6,7 @@ import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.mllib.util.MLUtils
 import org.apache.spark.rdd.RDD
 import org.apache.spark.mllib.linalg.{Matrix, Matrices}
+import org.apache.spark.mllib.linalg.distributed.RowMatrix
 
 
 object MllibTest {
@@ -14,22 +15,26 @@ object MllibTest {
     val conf = new SparkConf().setMaster("local[*]").setAppName("MllibTest")
     val sc = SparkContext.getOrCreate(conf)
 
-    val dv: Vector = Vectors.dense(2.0, 0.0, 8.0)
+    //val dv: Vector = Vectors.dense(2.0, 0.0, 8.0)
 
-    val sv1: Vector = Vectors.sparse(3, Array(0, 2), Array(2.0, 8.0))
+    //val sv1: Vector = Vectors.sparse(3, Array(0, 2), Array(2.0, 8.0))
 
-    val sv2: Vector = Vectors.sparse(3, Seq((0, 2.0), (2, 8.0)))
+    //val sv2: Vector = Vectors.sparse(3, Seq((0, 2.0), (2, 8.0)))
 
-    val pos: LabeledPoint = LabeledPoint(1.0, Vectors.dense(2.0, 0.0, 8.0))
+    //val pos: LabeledPoint = LabeledPoint(1.0, Vectors.dense(2.0, 0.0, 8.0))
 
-    val neg: LabeledPoint = LabeledPoint(0.0, Vectors.sparse(3, Array(0, 2), Array(2.0, 8.0)))
+    //val neg: LabeledPoint = LabeledPoint(0.0, Vectors.sparse(3, Array(0, 2), Array(2.0, 8.0)))
 
-    val examples: RDD[LabeledPoint] = MLUtils.loadLibSVMFile(sc, "D:\\Tools\\Spark\\spark-2.1.0-bin-hadoop2.6\\data\\mllib\\sample_libsvm_data.txt")
+    //val examples: RDD[LabeledPoint] = MLUtils.loadLibSVMFile(sc, "D:\\Tools\\Spark\\spark-2.1.0-bin-hadoop2.6\\data\\mllib\\sample_libsvm_data.txt")
 
-    val dm: Matrix = Matrices.dense(3, 2, Array(1.0, 3.0, 5.0, 2.0, 4.0, 6.0))
+    //val dm: Matrix = Matrices.dense(3, 2, Array(1.0, 3.0, 5.0, 2.0, 4.0, 6.0))
 
-    val sm: Matrix = Matrices.sparse(3, 2, Array(0, 1, 3), Array(0, 2, 1), Array(9, 6, 8))
+    //val sm: Matrix = Matrices.sparse(3, 2, Array(0, 1, 3), Array(0, 2, 1), Array(9, 6, 8))
 
-    print(sm)
+    val dv1 : Vector = Vectors.dense(1.0,2.0,3.0)
+    val dv2 : Vector = Vectors.dense(2.0,3.0,4.0)
+    val rows: RDD[Vector] = sc.parallelize(Array(dv1,dv2))
+    val mat : RowMatrix = new RowMatrix(rows)
+    mat.rows.foreach(println)
   }
 }
