a621ffd9330458bced72d39d44854b2f06634fbd
杨森 <564018929@qq.com>
2019-11-20 13:54:22 +0800

    add new

--- src/main/scala/com/mathartsys/yss/sparkmllib/MllibTest.scala
+++ src/main/scala/com/mathartsys/yss/sparkmllib/MllibTest.scala
@@ -5,8 +5,9 @@ import org.apache.spark.mllib.linalg.{Vector, Vectors}
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.mllib.util.MLUtils
 import org.apache.spark.rdd.RDD
-import org.apache.spark.mllib.linalg.{Matrix, Matrices}
-import org.apache.spark.mllib.linalg.distributed.RowMatrix
+import org.apache.spark.mllib.linalg.{Matrices, Matrix}
+import org.apache.spark.mllib.linalg.distributed.{IndexedRow, IndexedRowMatrix, RowMatrix}
+import org.apache.spark.mllib.linalg.distributed.{CoordinateMatrix, MatrixEntry}
 
 
 object MllibTest {
@@ -35,6 +36,57 @@ object MllibTest {
     val dv2 : Vector = Vectors.dense(2.0,3.0,4.0)
     val rows: RDD[Vector] = sc.parallelize(Array(dv1,dv2))
     val mat : RowMatrix = new RowMatrix(rows)
-    mat.rows.foreach(println)
+    val summary = mat.computeColumnSummaryStatistics()
+    //println(summary.count)
+    //最大向量
+    //println(summary.max)
+    //方差向量
+    //println(summary.variance)
+    //平均向量
+    //println(summary.mean)
+    //L1范数向量
+    //println(summary.normL1)
+    //L2范数向量
+    //println(summary.normL2)
+
+    val idxr1 = IndexedRow(1,dv1)
+    val idxr2 = IndexedRow(2,dv2)
+
+    val idxrows = sc.parallelize(Array(idxr1,idxr2))
+
+    val idxmat = new IndexedRowMatrix(idxrows)
+
+    //idxmat.rows.foreach(println)
+
+    //坐标矩阵CoordinateMatrix是一个基于矩阵项构成的RDD的分布式矩阵。
+    // 每一个矩阵项MatrixEntry都是一个三元组：
+    // (i: Long, j: Long, value: Double)，其中i是行索引，j是列索引，value是该位置的值。
+    // 坐标矩阵一般在矩阵的两个维度都很大，且矩阵非常稀疏的时候使用
+    val ent1 = new MatrixEntry(0,1,0.5)
+    val ent2 = new MatrixEntry(2,2,1.8)
+    val entries: RDD[MatrixEntry] = sc.parallelize(Array(ent1,ent2))
+    val coordMat = new CoordinateMatrix(entries)
+    //coordMat.entries.foreach(println)
+    //坐标矩阵可以通过transpose()方法对矩阵进行转置操作，
+    // 并可以通过自带的toIndexedRowMatrix()方法转换成索引行矩阵IndexedRowMatrix
+    val transMat = coordMat.transpose()
+    //transMat.entries.foreach(println)
+    val indexedRowMatrix = transMat.toIndexedRowMatrix()
+    //indexedRowMatrix.rows.foreach(println)
+    //分块矩阵
+    val ent3 = new MatrixEntry(2,0,-1)
+    val ent4 = new MatrixEntry(2,1,2)
+    val ent5 = new MatrixEntry(2,2,1)
+    val ent6 = new MatrixEntry(3,0,1)
+    val ent7 = new MatrixEntry(3,1,1)
+    val ent8 = new MatrixEntry(3,3,1)
+    val entries1 = sc.parallelize(Array(ent1,ent2,ent3,ent4,ent5,ent6,ent7,ent8))
+    //println(entries1.collect().foreach(println))
+    val coordMat1 = new CoordinateMatrix(entries1)
+    val matA = coordMat1.toBlockMatrix(2,2)
+    matA.validate()
+    println(matA.toLocalMatrix())
+    val ata = matA.transpose.multiply(matA)
+    println(ata.toLocalMatrix())
   }
 }
